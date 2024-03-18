0d1079c3ce98d0f09506b0864124f5e7ac940fed
radsadsoap <vinayaksh8432@gmail.com>
2024-03-18 11:55:58 +0530

    b

--- client/MSME/src/components/navbar.jsx
+++ client/MSME/src/components/navbar.jsx
@@ -1,15 +1,22 @@
-import Styles from "../styles/navbar.module.css";
+import styles from "../styles/navbar.module.css";
 
 export default function Navbar() {
     return (
         <>
-            <div className="header">
-                <h1 className="title">
-                    GOVERNMENT OF INDIA 
-                </h1>
-                <h1 className="ministry-title">
-                    MINISTRY OF MICRO, SMALL & MEDIUM ENTERPRISES, GOI
-                </h1>
+            <div className={styles.header}>
+                <div className={styles.govttitle}>
+                    <h3>भारत सरकार</h3>
+                    <h1>GOVERNMENT OF INDIA</h1>
+                </div>
+                <div className={styles.ministrytitle}>
+                    <h3>सूक्ष्म , लघु और मध्यम उद्यम मंत्रालय </h3>
+                    <h2>MINISTRY OF MICRO, SMALL & MEDIUM ENTERPRISES, GOI</h2>
+                </div>
+            </div>
+            <div className={styles.ministry}>
+                <h1>सूक्ष्म , लघु और मध्यम उद्यम मंत्रालय</h1>
+                <h2>Ministry of</h2>
+                <h3>Micro, small & Medium Enterprises</h3>
             </div>
         </>
     );
