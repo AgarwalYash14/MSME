4c9ca2099ac17cdd026612705a197fb437e83dda
radsadsoap <vinayaksh8432@gmail.com>
2024-03-18 10:40:53 +0530

    a

--- client/MSME/src/components/navbar.jsx
+++ client/MSME/src/components/navbar.jsx
@@ -1,8 +1,15 @@
+import Styles from "../styles/navbar.module.css";
+
 export default function Navbar() {
     return (
         <>
-            <div>
-                <p>dfghjkl</p>
+            <div className="header">
+                <h1 className="title">
+                    GOVERNMENT OF INDIA 
+                </h1>
+                <h1 className="ministry-title">
+                    MINISTRY OF MICRO, SMALL & MEDIUM ENTERPRISES, GOI
+                </h1>
             </div>
         </>
     );
