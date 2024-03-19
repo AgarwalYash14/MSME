88b6828d70d4aa65bde18bdc81f7addda8e246d4
Yash <kuchchhal.yash@gmail.com>
2024-03-19 13:25:58 +0530

    a

--- client/MSME/src/pages/homepage.jsx
+++ client/MSME/src/pages/homepage.jsx
@@ -9,9 +9,9 @@ export default function Homepage() {
                     <input type="search" name="" id="" />
                     <button>Create new Catalog</button>
                 </div>
-                <div>
+                <div className="section-2">
                     <div>
-                        <h2>Categories:</h2>
+                        <h2>Product Categories:</h2>
                         <select name="" id="">
                             <option value="HandcraftedProducts">
                                 Handcrafted Products
