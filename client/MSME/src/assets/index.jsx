5805fb1624719014b3a0eaf5b70e825829f3a493
Yash Agarwal <122815643+GitHub-YashAgarwal@users.noreply.github.com>
2024-05-03 17:13:51 +0530

    Delete client/MSME directory

--- client/MSME/src/assets/index.jsx
+++
@@ -1,6 +0,0 @@
-import emblem from "./Emblem.png";
-import g20 from "./G20.png";
-import swachh from "./Swachh-Bharat.png";
-import azadi from "./Azadi-Ka-Amrit-Mahotsav.png";
-
-export { emblem, g20, swachh, azadi };
